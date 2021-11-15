9d40cf7190940ccc8c158a14286eaf71d5e9524a
Sema Kırdar <sema@dijitaladam.com>
2021-11-15 12:12:38 +0300

    login controller created

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('admin.login');
+    }
+
+    public function store()
+    {
+
+    }
 }
---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+    <div class="login">
+
+    </div>
+@endsection()
